2bc717725315ff84e1938d6e20a856605e4c8113
vaishnavi-oss719 <rv172542@gmail.com>
2026-03-02 10:56:43 +0530

    updated status

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -1,56 +1,95 @@
+// // import jwt from 'jsonwebtoken';
+// // import dotenv from 'dotenv';
+// // import User from '../Models/user.schema.js';
+
+
+// //  dotenv.config();
+
+
+// //   const authMiddleware = async (req, res, next) => {
+// //     const token = req.header('Authorization')
+// //     if (!token) {
+// //         return res.status(401).json({message:"Token is missing"})
+// //     }
+// //     try {
+// //         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+// //         req.user = decoded;
+// //         const user = await User.findById(req.user._id);
+
+// //         if (user.role != 'admin') {
+// //             return res.status(401).json({ message: 'Access Deny' });
+// //         }
+// //         next();
+// //     } catch (error) {
+// //         res.status(500).json({message:'Invalid Token, Internal Server Error'})
+// //     }
+// // }
+
+// // export default authMiddleware;
+
+
+
 // import jwt from 'jsonwebtoken';
 // import dotenv from 'dotenv';
 // import User from '../Models/user.schema.js';
 
+// dotenv.config();
 
-//  dotenv.config();
+// const authMiddleware = async (req, res, next) => {
+//   const authHeader = req.header('Authorization');
 
+//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//     return res.status(401).json({ message: "Token is missing or invalid format" });
+//   }
 
-//   const authMiddleware = async (req, res, next) => {
-//     const token = req.header('Authorization')
-//     if (!token) {
-//         return res.status(401).json({message:"Token is missing"})
-//     }
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = decoded;
-//         const user = await User.findById(req.user._id);
+//   const token = authHeader.split(' ')[1];
 
-//         if (user.role != 'admin') {
-//             return res.status(401).json({ message: 'Access Deny' });
-//         }
-//         next();
-//     } catch (error) {
-//         res.status(500).json({message:'Invalid Token, Internal Server Error'})
-//     }
+//   try {
+//   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//   console.log("Decoded:", decoded);
+//   next();
+// } catch (error) {
+//   console.log("JWT Error:", error.message);
+//   return res.status(401).json({ message: error.message });
 // }
-
+// };
 // export default authMiddleware;
 
 
 
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import User from '../Models/user.schema.js';
+
+
+
+
+
+ import jwt from 'jsonwebtoken';
+  import dotenv from 'dotenv';
+
 
 dotenv.config();
 
-const authMiddleware = async (req, res, next) => {
-  const authHeader = req.header('Authorization');
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: "Token is missing or invalid format" });
-  }
-
-  const token = authHeader.split(' ')[1];
-
+const fetchUser = (req, res, next) => {
   try {
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log("Decoded:", decoded);
-  next();
-} catch (error) {
-  console.log("JWT Error:", error.message);
-  return res.status(401).json({ message: error.message });
-}
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1]; // IMPORTANT LINE
+
+    if (!token) {
+      return res.status(401).json({ message: "Token format invalid" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.user;
+    next();
+
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
 };
-export default authMiddleware;
+
+export default fetchUser;
