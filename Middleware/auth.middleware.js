4f54c080adcabb642094e93a9d8a50c624bd6956
vaishnavi-oss719 <rv172542@gmail.com>
2026-03-02 10:27:11 +0530

    updated status

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -1,28 +1,55 @@
+// import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+// import User from '../Models/user.schema.js';
+
+
+//  dotenv.config();
+
+
+//   const authMiddleware = async (req, res, next) => {
+//     const token = req.header('Authorization')
+//     if (!token) {
+//         return res.status(401).json({message:"Token is missing"})
+//     }
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = decoded;
+//         const user = await User.findById(req.user._id);
+
+//         if (user.role != 'admin') {
+//             return res.status(401).json({ message: 'Access Deny' });
+//         }
+//         next();
+//     } catch (error) {
+//         res.status(500).json({message:'Invalid Token, Internal Server Error'})
+//     }
+// }
+
+// export default authMiddleware;
+
+
+
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import User from '../Models/user.schema.js';
 
+dotenv.config();
 
- dotenv.config();
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.header('Authorization');
 
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: "Token is missing or invalid format" });
+  }
 
-  const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')
-    if (!token) {
-        return res.status(401).json({message:"Token is missing"})
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        const user = await User.findById(req.user._id);
+  const token = authHeader.split(' ')[1];
 
-        if (user.role != 'admin') {
-            return res.status(401).json({ message: 'Access Deny' });
-        }
-        next();
-    } catch (error) {
-        res.status(500).json({message:'Invalid Token, Internal Server Error'})
-    }
-}
-
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid Token' });
+  }
+};
 export default authMiddleware;
